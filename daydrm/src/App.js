2b60511b5866d76ec8093c37a0cbc3d9d0caaaaf
Noe Garcia <garcia.noav@gmail.com>
2024-10-09 21:35:18 -0600

    feat: update .gitignore

--- daydrm/src/App.js
+++ daydrm/src/App.js
@@ -2,16 +2,13 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [backendUsers, setBackendUsers] = useState([{}]);
-
+  
+  // render when the component is loaded the first time
   useEffect(() => {
     fetch("/").then(
-      response => {
-        response.json()
-      }
+      response => response.json() // convert to json
     ).then(
-      data => {
-        setBackendUsers(data)
-      }
+      data => setBackendUsers(data) // store within state variable
     );
   }, []);
 
